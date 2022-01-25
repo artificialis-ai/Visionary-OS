744eb4f761f617f5b4a983e4acd632dc9c6bbd68
KliKli2 <maximilian.hohenbuehel@gmail.com>
2022-01-25 17:56:30 +0100

    Primary setup, ready to fork

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", case_insensitive=True)
+
+# EVENT EXAMPLES
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord')
+    members = len(set(bot.get_all_members()))
+    game = discord.Game("Open Sourcerers")
+    await bot.change_presence(activity = game)
+
+@client.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$ping'):
+        await message.channel.send('Hello!')
+
+# COMMAND EXAMPLES
+@bot.command(name='command_name', description="description for help command")
+async def command(ctx, other_arguments_here):
+    # Do stuff...
+
+bot.run(os.environ['BOT_TOKEN'])
